60c4266fea86237123657b2d782b6708126eb07b
dawnight <weizhiqimail@foxmail.com>
2019-10-21 22:52:24 +0800

    添加使用 jsx 语法

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,20 @@
         <h3>这是第一块区域</h3>
       </el-col>
     </el-row>
+    <anchor :level="1"></anchor>
   </div>
 </template>
 
 <script>
 import ElRow from './components/el-row';
 import ElCol from './components/el-row';
+import Anchor from './components/anchor';
 export default {
   name: 'app',
   components: {
     ElRow,
-    ElCol
+    ElCol,
+    Anchor
   }
 }
 </script>
---
+++ src/components/anchor/index.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "anchor",
+  data() {
+    const count = 6;
+    let levelList = [];
+    for (let i = 0; i < count; i++) {
+      levelList.push(i);
+    }
+    return {
+      levelList
+    };
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  render(h) {
+    return (<div>
+      <h1>Anchor Component</h1>
+    </div>)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
